61bc9372d211e6b88914b97e332681ef1e0e6bd0
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-10 21:27:23 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # deno_module_server
+
 Hosts Deno modules bundled for the web.
+
+## Example
+
+```ts
+import { parse } from "http://module-server.deno.dev/?specifier=https://deno.land/x/boolean/mod.ts";
+
+console.log(parse("true"));
+```
+
+---
+
+Developed with [🦕](https://deno.land/) by
+[**@EthanThatOneKid**](https://etok.codes/)
---
+++ main.ts
@@ -0,0 +1,37 @@
+import { build, type Format } from "esbuild";
+import { denoPlugins } from "@luca/esbuild-deno-loader";
+
+if (import.meta.main) {
+  Deno.serve(handle);
+}
+
+async function handle(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  if (url.pathname !== "/" && request.method !== "GET") {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const specifier = url.searchParams.get("specifier");
+  if (!specifier) {
+    return new Response("Missing specifier", { status: 400 });
+  }
+
+  const format = (url.searchParams.get("format") || "esm") as Format;
+  const result = await build({
+    entryPoints: [specifier],
+    bundle: true,
+    format,
+    plugins: [...denoPlugins()],
+    write: false,
+  });
+  const code = result.outputFiles.find((file) => file.path === "<stdout>");
+  if (!code) {
+    return new Response("No output", { status: 500 });
+  }
+
+  return new Response(code.text, {
+    headers: {
+      "content-type": "application/javascript",
+    },
+  });
+}
